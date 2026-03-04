8b5ffb4a31fc34b4d054079329acaa39bab4ed2a
zhishi <1951671751@qq.com>
2026-03-04 18:23:35 +0800

    恢复

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,24 +17,24 @@ const router = createRouter({
     {
       path: "/workbench",
       component: () => import("@/pages/workbench/index.vue"),
-      // redirect: "/project",
+      redirect: "/project",
       children: [
-        // {
-        //   path: "/project",
-        //   component: () => import("@/views/project/index.vue"),
-        // },
-        // {
-        //   path: "/projectDetail",
-        //   component: () => import("@/views/projectDetail/index.vue"),
-        // },
-        // {
-        //   path: "/setting",
-        //   component: () => import("@/views/setting/index.vue"),
-        // },
-        // {
-        //   path: "/taskList",
-        //   component: () => import("@/views/taskList/index.vue"),
-        // },
+        {
+          path: "/project",
+          component: () => import("@/views/project/index.vue"),
+        },
+        {
+          path: "/projectDetail",
+          component: () => import("@/views/projectDetail/index.vue"),
+        },
+        {
+          path: "/setting",
+          component: () => import("@/views/setting/index.vue"),
+        },
+        {
+          path: "/taskList",
+          component: () => import("@/views/taskList/index.vue"),
+        },
       ],
     },
     {
