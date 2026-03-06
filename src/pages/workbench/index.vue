1276b121c18d88f8c208b054e3791d5a55a0157f
ACT丶流星雨 <1340145680@qq.com>
2026-03-06 15:14:49 +0800

    更新完侧边栏

--- src/pages/workbench/index.vue
+++ src/pages/workbench/index.vue
@@ -13,10 +13,11 @@
           :showArrow="false"
           v-for="(menu, index) in menuList"
           :key="index">
-          <div class="item c" :class="{ active: activeMenu == menu.path }" @click="handleClick(menu.path)">
+          <div class="item c" v-if="menu.type === 'btn'" :class="{ active: activeMenu == menu.path }" @click="handleClick(menu.path)">
             <component :is="menu.icon" class="icon" />
             <span v-show="!collapsed">{{ menu.label }}</span>
           </div>
+          <div class="divider" v-if="menu.type === 'divider'"></div>
         </t-tooltip>
       </div>
       <div class="footItem fc ac">
@@ -43,11 +44,19 @@
 
 <script setup lang="ts">
 const menuList = [
-  { path: "/project", label: "我的项目", icon: "i-folder-close" },
-  { path: "/taskList", label: "任务中心", icon: "i-view-list" },
+  { type: "btn", path: "/project", label: "我的项目", icon: "i-folder-close" },
+  { type: "btn", path: "/taskList", label: "任务中心", icon: "i-view-list" },
+  { type: "divider" },
+  { type: "btn", path: "/setting", label: "项目总览", icon: "i-more-app" },
+  { type: "btn", path: "/setting", label: "小说原文", icon: "i-notebook" },
+  { type: "btn", path: "/setting", label: "剧本Agent", icon: "i-color-filter" },
+  { type: "btn", path: "/setting", label: "剧本管理", icon: "i-document-folder" },
+  { type: "btn", path: "/setting", label: "视频生产", icon: "i-carousel-video" },
+  { type: "divider" },
+  { type: "btn", path: "/setting", label: "资产中心", icon: "i-receive" },
 ];
 
-const footMenuList = [{ path: "/setting", label: "设置", icon: "i-setting" }];
+const footMenuList = [{ type: "btn", path: "/setting", label: "设置", icon: "i-setting" }];
 
 const router = useRouter();
 const route = useRoute();
@@ -93,7 +102,8 @@ function handleClick(value: string | number) {
       width: 100%;
       height: 100%;
       .item {
-        margin-bottom: 8px;
+        margin-bottom: 4px;
+        margin-top: 4px;
         cursor: pointer;
         width: 50px;
         height: 50px;
@@ -109,6 +119,12 @@ function handleClick(value: string | number) {
         background-color: #000 !important;
         color: #ebebeb;
         border-radius: 16px;
+      }
+      .divider {
+        width: 50px;
+        height: 1px;
+        background-color: #ecedef;
+        margin: 8px 0;
       }
     }
     .footItem {
@@ -139,7 +155,7 @@ function handleClick(value: string | number) {
     margin-left: 1rem;
     background-color: #fff;
     border-radius: 16px;
-     width: 100%;
+    width: 100%;
     height: 100%;
     overflow-x: hidden;
     overflow-y: auto;
